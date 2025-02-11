a8237093e090090aead8982db71c29829f2e3d7c
gladhee <gladhee@icloud.com>
2025-02-11 11:52:52 +0900

    feat(Application): 타이머 추가

--- programming_practice/week-2/day6/Application.java
+++ programming_practice/week-2/day6/Application.java
@@ -1,9 +1,15 @@
 public class Application {
 
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         Sudoku sudoku = new Sudoku();
         sudoku.solve();
         sudoku.printBoard();
+        long endTime = System.currentTimeMillis(); // 코드 끝난 시간
+
+        long durationTimeSec = endTime - startTime;
+        System.out.println(durationTimeSec + "m/s"); // 밀리세컨드 출력
+        System.out.println((durationTimeSec / 1000) + "sec"); // 초 단위 변환 출력
     }
 
 }
