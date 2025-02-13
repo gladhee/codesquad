09fc60cf555711a7fa0288e041127fc68cc17fc1
gladhee <gladhee@icloud.com>
2025-02-13 15:33:22 +0900

    feat(Hanoi): 하노이 탑 구현

---
+++ programming_practice/week-2/day9/Hanoi.java
@@ -0,0 +1,70 @@
+import java.util.List;
+
+public class Hanoi {
+
+    private static final int MAX_WIDTH = 10;
+
+    private final Desk[] desks;
+    private final int target;
+    private int current;
+
+    public Hanoi(int target) {
+        this.desks = new Desk[3];
+        for (int i = 0; i < 3; i++) {
+            this.desks[i] = new Desk(i + 1);
+        }
+        desks[0].push(Parts.COMPUTE);
+        desks[0].push(Parts.DISPLAY);
+        desks[0].push(Parts.DRV);
+        this.target = Math.min(target, 8);
+        this.current = 1;
+    }
+
+    public void solve() {
+        if (this.target == 1) {
+            return;
+        }
+        move(3, desks[0], desks[1], desks[2]);
+    }
+
+    private boolean move(int n, Desk from, Desk aux, Desk to) {
+        if (n == 1) {
+            return movePart(from, to);
+        }
+        else {
+            if (move(n - 1, from, to, aux)) {
+                return true;
+            }
+            if (movePart(from, to)) {
+                return true;
+            }
+            return move(n - 1, aux, from, to);
+        }
+    }
+
+    private boolean movePart(Desk from, Desk to) {
+        Parts part = from.pop();
+        to.push(part);
+        current++;
+
+        return current == target;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Step ").append(this.current).append("\n");
+
+        for (int i = 0; i < MAX_WIDTH; ++i) {
+            sb.append(String.join(" ", List.of(
+                    this.desks[0].toList().get(i),
+                    this.desks[1].toList().get(i),
+                    this.desks[2].toList().get(i)
+            )));
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+
+}
