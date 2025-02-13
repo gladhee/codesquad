2b560476e3c43761cf6bc24a7d1dcf967a2f48c6
gladhee <gladhee@icloud.com>
2025-02-13 15:33:37 +0900

    feat(Input): 시뮬레이션 횟수 받을 Input 클래스 구현

---
+++ programming_practice/week-2/day9/Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Input {
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static int nextInt() {
+        return sc.nextInt();
+    }
+
+}
