8330dfd031f205bf6f8e6a26f6a184cc5e83a8fe
gladhee <gladhee@icloud.com>
2025-02-15 21:58:10 +0900

    refactor(Input): nextLine -> prompt 수정 및 "SQL >" 프롬프트 추가

--- programming_practice/week-2/day10/Input.java
+++ programming_practice/week-2/day10/Input.java
@@ -4,8 +4,24 @@ public class Input {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static String nextLine() {
+    public static String prompt() {
+        System.out.println("SQL> ");
         return scanner.nextLine();
     }
 
 }
+
+/*
+*
+*
+        //        String input = """
+//            CREATE TABLE billboard (singer String, year Numeric, song String);
+//            INSERT INTO billboard (singer, year, song) VALUES ("BTS", 2020, "Dynamite");
+//            INSERT INTO billboard (singer) VALUES ("BTS");
+//            UPDATE billboard SET singer = "BTS", year = 2021 WHERE year = 2020;
+//            DELETE FROM billboard WHERE singer = "BTS";
+//            DROP TABLE billboard;
+//            """;
+        String input = "CREATE TABLE billboard (singer String, year Numeric, song String);";
+//        String input = "INSERT INTO billboard (singer, year, song) VALUES (\"BTS\", 2020, \"Dynamite\");";
+* */
