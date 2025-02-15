e52691b8b9f393ac7e5739a293655f8f732cc07d
gladhee <gladhee@icloud.com>
2025-02-15 21:38:12 +0900

    feat(Input): 표준입력 담당 클래스 작성

---
+++ programming_practice/week-2/day10/Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Input {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String nextLine() {
+        return scanner.nextLine();
+    }
+
+}
