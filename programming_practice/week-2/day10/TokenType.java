6347559be640409fdf124a408414183732017135
gladhee <gladhee@icloud.com>
2025-02-15 09:44:52 +0900

    feat(TokenType): Enum 토큰 타입 정의

---
+++ programming_practice/week-2/day10/TokenType.java
@@ -0,0 +1,31 @@
+public enum TokenType {
+
+    // Keyword
+    CREATE,
+    TABLE,
+    INSERT,
+    INTO,
+    VALUES,
+    UPDATE,
+    SET,
+    DELETE,
+    FROM,
+    WHERE,
+
+    // Symbol
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    COMMA,
+    SEMICOLON,
+    EQUALS,
+    GREATER,
+    LESS,
+    DOUBLE_QUOTE,
+
+    // literal
+    IDENTIFIER,
+
+    // End of Input
+    EOF
+
+}
