aad62e8f0f98dbe11777bfa0ff87f3378c85c23c
gladhee <gladhee@icloud.com>
2025-02-18 21:01:36 +0900

    feat(Application): 프로그램 진입점 작성

---
+++ programming_practice/week-3/day12/triangle_area/Application.java
@@ -0,0 +1,12 @@
+public class Application {
+
+    public static void main(String[] args) {
+        Input input = new Input();
+        DiagramFactory diagramFactory = new DiagramFactory(input);
+        Diagram diagram = diagramFactory.createDiagramByInput();
+        Plane plane = new Plane(diagram);
+
+        System.out.println(plane);
+    }
+
+}
