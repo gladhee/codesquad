ee38e38210b0ce0bf25b0d564d4ec4b082306da2
gladhee <gladhee@icloud.com>
2025-02-21 18:54:08 +0900

    refactor(PrimeAlpha): functional refactor

--- programming_practice/week-3/day15/PrimeAlpha.java
+++ programming_practice/week-3/day15/PrimeAlpha.java
@@ -1,39 +1,17 @@
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 public class PrimeAlpha {
-    private int number;
 
-    public PrimeAlpha(int number) {
-        this.number = number;
-    }
-
-    public boolean isPrime() {
-        Set<Integer> primeSet = new HashSet<Integer>(){ {add(1); add(number);} };
-        return number > 1 && factors().equals(primeSet);
-    }
-
-    public boolean isFactor(int potentialFactor) {
-        return number % potentialFactor == 0;
-    }
-
-    public Set<Integer> factors() {
-        HashSet<Integer> factors = new HashSet<>();
-        for (int pod=1; pod <= Math.sqrt(number); pod++) {
-            if (isFactor(pod)) {
-                factors.add(pod);
-                factors.add(number / pod);
-            }
-        }
-        return factors;
-    }
+    public Predicate<Integer> isPrime = n ->
+            n >= 2 && IntStream.rangeClosed(2, (int) Math.sqrt(n))
+                    .allMatch(i -> n % i != 0);
 
     public static void main(String[] args) {
-        PrimeAlpha prime1 = new PrimeAlpha(10);
-        PrimeAlpha prime2 = new PrimeAlpha(7);
+        PrimeAlpha prime = new PrimeAlpha();
 
-        System.out.println(prime1.isPrime());
-        System.out.println(prime2.isPrime());
+        System.out.println(prime.isPrime.test(10));
+        System.out.println(prime.isPrime.test(7));
     }
+
 }
