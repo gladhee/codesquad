92ca641683c6e21f95bd9c11e00a0de182c06097
gladhee <gladhee@icloud.com>
2025-02-24 14:58:05 +0900

    feat(DirectionalReadInstruction): Queue 에 순서대로 삽입후 반환 로직 작성

--- programming_practice/week-4/day16/DirectionalReadInstruction.java
+++ programming_practice/week-4/day16/DirectionalReadInstruction.java
@@ -7,6 +7,40 @@ public class DirectionalReadInstruction {
     private DirectionalReadInstruction() {
     }
 
+    public static Queue<MoveReader> getMoveReaders() {
+        Queue<MoveReader> queue = new LinkedList<>();
+
+        queue.add((out -> readStart(20, 20, out)));
+        queue.add(out -> readUp(20, 18, out));
+        queue.add(out -> readUp(20, 14, out));
+        queue.add(out -> readUpLeft(20, 10, out));
+        queue.add(out -> readDown(18, 11, out));
+        queue.add(out -> readDown(18, 15, out));
+        queue.add(out -> readDownLeft(18, 19, out));
+        queue.add(out -> readUp(16, 18, out));
+        queue.add(out -> readUp(16, 14, out));
+        queue.add(out -> readUpLeft(16, 10, out));
+        queue.add(out -> readDown(14, 11, out));
+        queue.add(out -> readDown(14, 15, out));
+        queue.add(out -> readDownLeft(14, 19, out));
+        queue.add(out -> readUp(12, 18, out));
+        queue.add(out -> readUp(12, 14, out));
+        queue.add(out -> readUpLeft(12, 10, out));
+        queue.add(out -> readUp(12, 5, out));
+        queue.add(out -> readUpLeft(12, 1, out));
+        queue.add(out -> readDown(10, 2, out));
+        queue.add(out -> readDown(10, 7, out));
+        queue.add(out -> readDown(10, 11, out));
+        queue.add(out -> readDown(10, 15, out));
+        queue.add(out -> readEnd(10, 19, out));
+        queue.add(out -> readUp(8, 12, out));
+        queue.add(out -> readDown(5, 9, out));
+        queue.add(out -> readUp(3, 12, out));
+        queue.add(out -> readDown(1, 9, out));
+
+        return queue;
+    }
+
     public static void readStart(int startX, int startY, List<Point> outPoints) {
         outPoints.add(new Point(startX, startY));
         outPoints.add(new Point(startX - 1, startY));
