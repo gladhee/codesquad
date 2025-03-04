73fca05adde4ab73899d27e1afafc9119ef0c0c6
gladhee <gladhee@icloud.com>
2025-03-04 14:04:55 +0900

    feat(Pawn): implement Pawn class with color attribute

---
+++ app/src/main/java/org/chess/domain/piece/Pawn.java
@@ -0,0 +1,15 @@
+package org.chess.domain.piece;
+
+public class Pawn {
+
+    private final String color;
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+}
