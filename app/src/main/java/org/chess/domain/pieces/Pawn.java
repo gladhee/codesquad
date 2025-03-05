c7c0e070216a187aa631aa411df61dafe1a5687d
gladhee <gladhee@icloud.com>
2025-03-05 10:47:27 +0900

    feat(Pawn): add representation field and update constructor

--- app/src/main/java/org/chess/domain/pieces/Pawn.java
+++ app/src/main/java/org/chess/domain/pieces/Pawn.java
@@ -8,17 +8,24 @@ public class Pawn {
     public static final char BLACK_REPRESENTATION = 'P';
 
     private final String color;
+    private final char representation;
 
     public Pawn() {
         this.color = WHITE_COLOR;
+        this.representation = WHITE_REPRESENTATION;
     }
 
-    public Pawn(String color) {
+    public Pawn(String color, char representation) {
         this.color = color;
+        this.representation = representation;
     }
 
     public String getColor() {
         return color;
+    }
+
+    public char getRepresentation() {
+        return representation;
     }
 
 }
