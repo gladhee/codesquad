37b474b0ee4c8f3d4a8230f2f84af76d30496894
gladhee <gladhee@icloud.com>
2025-03-05 22:47:05 +0900

    refactor(Piece): enhance piece creation with specific methods for each type

--- app/src/main/java/org/chess/domain/pieces/Piece.java
+++ app/src/main/java/org/chess/domain/pieces/Piece.java
@@ -4,10 +4,18 @@ public class Piece {
 
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
+
+    public static final String PAWN_NAME = "pawn";
+    public static final String QUENN_NAME = "queen";
+    public static final String KING_NAME = "king";
+    public static final String ROOK_NAME = "rook";
+    public static final String BISHOP_NAME = "bishop";
+    public static final String KNIGHT_NAME = "knight";
+
     public static final char WHITE_PAWN_REPRESENTATION = 'p';
     public static final char BLACK_PAWN_REPRESENTATION = 'P';
-    public static final char WHITE_QUENN_REPRESENTATION = 'q';
-    public static final char BLACK_QUENN_REPRESENTATION = 'Q';
+    public static final char WHITE_QUEEN_REPRESENTATION = 'q';
+    public static final char BLACK_QUEEN_REPRESENTATION = 'Q';
     public static final char WHITE_KING_REPRESENTATION = 'k';
     public static final char BLACK_KING_REPRESENTATION = 'K';
     public static final char WHITE_ROOK_REPRESENTATION = 'r';
@@ -17,12 +25,66 @@ public class Piece {
     public static final char WHITE_KNIGHT_REPRESENTATION = 'n';
     public static final char BLACK_KNIGHT_REPRESENTATION = 'N';
 
+    private final String name;
     private final String color;
     private final char representation;
 
-    public Piece(String color, char representation) {
+    private Piece(String name, String color, char representation) {
+        this.name = name;
         this.color = color;
         this.representation = representation;
+    }
+
+    public static Piece createWhitePawn() {
+        return new Piece(PAWN_NAME, WHITE_COLOR, WHITE_PAWN_REPRESENTATION);
+    }
+
+    public static Piece createBlackPawn() {
+        return new Piece(PAWN_NAME, BLACK_COLOR, BLACK_PAWN_REPRESENTATION);
+    }
+
+    public static Piece createWhiteQuenn() {
+        return new Piece(QUENN_NAME, WHITE_COLOR, WHITE_QUEEN_REPRESENTATION);
+    }
+
+    public static Piece createBlackQuenn() {
+        return new Piece(QUENN_NAME, BLACK_COLOR, BLACK_QUEEN_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(KING_NAME, WHITE_COLOR, WHITE_KING_REPRESENTATION);
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(KING_NAME, BLACK_COLOR, BLACK_KING_REPRESENTATION);
+    }
+
+    public static Piece createWhiteRook() {
+        return new Piece(ROOK_NAME, WHITE_COLOR, WHITE_ROOK_REPRESENTATION);
+    }
+
+    public static Piece createBlackRook() {
+        return new Piece(ROOK_NAME, BLACK_COLOR, BLACK_ROOK_REPRESENTATION);
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(BISHOP_NAME, WHITE_COLOR, WHITE_BISHOP_REPRESENTATION);
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(BISHOP_NAME, BLACK_COLOR, BLACK_BISHOP_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKnight() {
+        return new Piece(KNIGHT_NAME, WHITE_COLOR, WHITE_KNIGHT_REPRESENTATION);
+    }
+
+    public static Piece createBlackKnight() {
+        return new Piece(KNIGHT_NAME, BLACK_COLOR, BLACK_KNIGHT_REPRESENTATION);
+    }
+
+    public String getName() {
+        return name;
     }
 
     public String getColor() {
