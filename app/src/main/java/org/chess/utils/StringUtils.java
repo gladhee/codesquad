013ddf45819b5ce9579ae8245ea008a20b059b31
gladhee <gladhee@icloud.com>
2025-03-05 22:18:37 +0900

    feat(StringUtils): add StringUtils class for newline handling

---
+++ app/src/main/java/org/chess/utils/StringUtils.java
@@ -0,0 +1,14 @@
+package org.chess.utils;
+
+public class StringUtils {
+
+    public static final String NEWLINE = System.lineSeparator();
+
+    private StringUtils() {
+    }
+
+    public static String appendNewLine(String str) {
+        return str + NEWLINE;
+    }
+
+}
