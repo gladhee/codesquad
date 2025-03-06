4041d4edbbbcd9e4067f468aafab3c4968b05c9a
gladhee <gladhee@icloud.com>
2025-03-06 10:32:13 +0900

    test(BoardTest): add all pieces test

--- app/src/test/java/org/chess/domain/board/BoardTest.java
+++ app/src/test/java/org/chess/domain/board/BoardTest.java
@@ -1,44 +1,78 @@
 package org.chess.domain.board;
 
 import org.chess.domain.pieces.Piece;
+import org.chess.utils.StringUtils;
 import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.chess.utils.StringUtils.appendNewLine;
 
 class BoardTest {
 
-    private Piece whitePiece;
-    private Piece blackPiece;
+    private Piece whitePawn;
+    private Piece blackPawn;
 
     @BeforeEach
     void setUp() {
-        this.whitePiece = new Piece(Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
-        this.blackPiece = new Piece(Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
+        this.whitePawn = Piece.createWhitePawn();
+        this.blackPawn = Piece.createBlackPawn();
     }
 
     @Test
     @DisplayName("보드 생성 후 Pawn을 추가하고 저장할 수 있어야 한다")
     void create() throws Exception {
+        // given
         Board board = new Board();
 
-        verifyAddedPawnInBoard(board, whitePiece, 1);
-        verifyAddedPawnInBoard(board, blackPiece, 2);
-    }
+        // when
+        board.add(whitePawn);
 
-    void verifyAddedPawnInBoard(Board board, Piece piece, int count) {
-        board.add(piece);
-        assertEquals(count, board.size());
-        assertEquals(piece, board.findPawn(count - 1));
+        // then
+        assertThat(1).isEqualTo(board.size());
+        assertThat(whitePawn).isEqualTo(board.findPawn(0));
+
+        // when
+        board.add(blackPawn);
+
+        // then
+        assertThat(2).isEqualTo(board.size());
+        assertThat(blackPawn).isEqualTo(board.findPawn(1));
     }
 
     @Test
     @DisplayName("보드 생성 후 폰을 초기화시 흰색 폰과 검은색 폰이 각각 8개씩 생성되어야 한다")
     void initialize() throws Exception {
+        // given
         Board board = new Board();
+
+        // when
         board.initialize();
+
+        // then
         assertThat("pppppppp").isEqualTo(board.getPawnsResultWith(6));
         assertThat("PPPPPPPP").isEqualTo(board.getPawnsResultWith(1));
+    }
+
+    @Test
+    @DisplayName("보드 initailize 후 모든 기물이 생성되어야 한다")
+    void initializeAllPieces() throws Exception {
+        // given
+        Board board = new Board();
+
+        // when
+        board.initialize();
+
+        // then
+        assertThat(board.pieceCount()).isEqualTo(32);
+
+        String blankRank = appendNewLine("........");
+
+        assertThat(appendNewLine("RNBQKBNR") +
+                appendNewLine("PPPPPPPP") +
+                blankRank + blankRank + blankRank + blankRank +
+                appendNewLine("pppppppp") +
+                appendNewLine("rnbqkbnr"))
+                .isEqualTo(board.showBoard());
     }
 
 }
