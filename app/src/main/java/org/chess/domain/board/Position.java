e2a310525a890b5539f86a878257e83a9fd7a76e
gladhee <gladhee@icloud.com>
2025-03-08 22:35:13 +0900

    feat(Position): update position calculation and add equality/hashCode methods
    - equals(): boolean override
    - hashCode(): int override

--- app/src/main/java/org/chess/domain/board/Position.java
+++ app/src/main/java/org/chess/domain/board/Position.java
@@ -1,5 +1,7 @@
 package org.chess.domain.board;
 
+import java.util.Objects;
+
 public class Position {
 
     private static final int BOARD_SIZE = 8;
@@ -14,16 +16,34 @@ public class Position {
 
     public static Position of(String pos) {
         int x = pos.charAt(0) - 'a';
-        int y = pos.charAt(1) - '1';
+
+        // 8 -> 0, 7 -> 1, ..., 1 -> 7
+        int y = BOARD_SIZE - pos.charAt(1) + '0';
+        return new Position(y, x);
+    }
+
+    public static Position of(int y, int x) {
         return new Position(y, x);
     }
 
     public int y() {
-        return BOARD_SIZE - y - 1;
+        return y;
     }
 
     public int x() {
         return x;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Position position)) return false;
+        return y == position.y && x == position.x;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(y, x);
     }
 
 }
