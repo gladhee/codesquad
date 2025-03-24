76d5dbd91a0ac9277f875660768eb04ca3a689fe
gladhee <gladhee@icloud.com>
2025-03-24 13:50:02 +0900

    feat(GithubDashboardApplication): enable Feign clients for GitHub API integration

--- backend-class/be-github-dashboard/src/main/java/glad/github_dashboard/GithubDashboardApplication.java
+++ backend-class/be-github-dashboard/src/main/java/glad/github_dashboard/GithubDashboardApplication.java
@@ -2,8 +2,10 @@ package glad.github_dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class GithubDashboardApplication {
 
 	public static void main(String[] args) {
