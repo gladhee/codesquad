8d2f661f73e962f09dfca23a83f79483ced0f8e1
gladhee <gladhee@icloud.com>
2025-03-25 00:27:50 +0900

    feat(GithubClient): organization 레포지토리 목록 불러오는 Github API 요청 추가

--- backend-class/be-github-dashboard/src/main/java/glad/github_dashboard/client/GithubClient.java
+++ backend-class/be-github-dashboard/src/main/java/glad/github_dashboard/client/GithubClient.java
@@ -1,6 +1,7 @@
 package glad.github_dashboard.client;
 
 import glad.github_dashboard.dto.PullRequest;
+import glad.github_dashboard.dto.RepositoryInfo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,9 @@ import java.util.List;
 
 @FeignClient(name = "githubClient", url = "${github.api-url}", configuration = GithubClientConfig.class)
 public interface GithubClient {
+
+    @GetMapping("/orgs/{org}/repos")
+    List<RepositoryInfo> getRepositories(@PathVariable String org);
 
     @GetMapping("/repos/{owner}/{repo}/pulls?state=all&per_page=100")
     List<PullRequest> getPullRequests(@PathVariable("owner") String owner, @PathVariable("repo") String repo);
